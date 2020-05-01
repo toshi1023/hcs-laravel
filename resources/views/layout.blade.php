9e2c515c0572895794d2eff2f85024961cd1db69
EC2 Default User <ec2-user@ip-10-0-10-133.ap-northeast-1.compute.internal>
2020-05-01 02:48:08 +0000

    First Commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HitcHike Community Space</title>
+    <!-- cssの呼び出し -->
+    <link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <div class="title">
+        <h1>HitcHike Community Space</h1>
+    </div>
+    <nav class="menubar">
+        <ul>
+            <li><a href="#">TOP</a></li>
+            <li><a href="#">記事一覧</a></li>
+            <li><a href="#">友達検索</a></li>
+            <li><a href="#">ログイン</a></li>
+        </ul>
+    </nav>
+
+</header>
+    <p>Hello World</p>
+    <button type="submit" class="btn btn-primary">送信</button>
+</body>
+</html>
