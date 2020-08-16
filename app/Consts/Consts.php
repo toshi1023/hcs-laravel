2a64452b33cc6bd4e3fa185cc472a1a3ca1be8e7
toshi1023 <toshimura1992@gmail.com>
2020-08-16 22:56:42 +0900

    FileUpload modified

--- app/Consts/Consts.php
+++ app/Consts/Consts.php
@@ -12,5 +12,5 @@ class Consts
     const LOGIN_URL = 'login';
 
     // 登録無しのプロフィール画像URL
-    const NO_IMAGE = 'https://aws-hcs-image.s3-ap-northeast-1.amazonaws.com/User/no_image.png';
+    const NO_IMAGE = env('AWS_NOIMAGE');
 }
