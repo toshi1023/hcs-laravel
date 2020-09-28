27211a085d83e371c65bd2eda7b25df3347f6b83
toshi1023 <toshimura1992@gmail.com>
2020-09-28 08:13:24 +0000

    laravel passport modify

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Api\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\JsonResponse;
 
 class LoginController extends Controller
 {
@@ -17,12 +18,26 @@ class LoginController extends Controller
 
         // $credentials = $request->only('email', 'password');
 
-        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
-            $accessToken = Auth::user()->createToken('authToken')->accessToken;
-            return response(['user' => Auth::user(), 'token' => $accessToken]);
+        // if(Auth::attempt(['email' => $request->email, 'password' => $request->password])) {
+        //     $accessToken = Auth::user()->createToken('authToken')->accessToken;
+        //     return response(['user' => Auth::user(), 'token' => $accessToken]);
+        // }
+
+        // return response([
+        //     'message' => 'Unauthenticated.'
+        // ], 401);
+
+        $credentials = $request->only('email', 'password');
+
+        // 認証成功時はtokenを返す
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('Laravel Password Grant Client')->accessToken;
+            return new JsonResponse(['token' => $token], 200);
         }
 
-        return response([
+        // 認証失敗時はエラーメッセージを返す
+        return new JsonResponse([
             'message' => 'Unauthenticated.'
         ], 401);
     }
