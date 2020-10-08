1bc12be032da574d550db0bb0863d63d7e9fc3f4
toshi1023 <toshimura1992@gmail.com>
2020-10-08 23:06:35 +0900

    dateSelects modified

--- resources/js/components/parts/appBar.js
+++ resources/js/components/parts/appBar.js
@@ -1,205 +1,232 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import {AppBar, Toolbar, IconButton, Typography, Badge, MenuItem, Menu} from '@material-ui/core';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import MailIcon from '@material-ui/icons/Mail';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import {
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Badge,
+    MenuItem,
+    Menu
+} from "@material-ui/core";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MailIcon from "@material-ui/icons/Mail";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import MoreIcon from "@material-ui/icons/MoreVert";
 import styled from "styled-components";
-import MenuDrawer from './drawer';
-import { withRouter } from 'react-router-dom/cjs/react-router-dom.min';
+import MenuDrawer from "./drawer";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
+import { useHistory } from "react-router-dom";
 
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    backgroundColor: '#1b2538',
-  },
-  grow: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    [theme.breakpoints.up('sm')]: {
-      display: 'flex',
+const useStyles = makeStyles(theme => ({
+    appBar: {
+        backgroundColor: "#1b2538"
     },
-  },
-  sectionDesktop: {
-    display: 'none',
-    [theme.breakpoints.up('md')]: {
-      display: 'flex',
+    grow: {
+        flexGrow: 1
     },
-  },
-  sectionMobile: {
-    display: 'flex',
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
+    menuButton: {
+        marginRight: theme.spacing(2)
     },
-  },
-  badge: {
-    marginRight: theme.spacing(1),
-  },
+    title: {
+        padding: theme.spacing(2),
+        display: "flex",
+        [theme.breakpoints.up("sm")]: {
+            display: "flex"
+        }
+    },
+    sectionDesktop: {
+        display: "none",
+        [theme.breakpoints.up("md")]: {
+            display: "flex"
+        }
+    },
+    sectionMobile: {
+        display: "flex",
+        [theme.breakpoints.up("md")]: {
+            display: "none"
+        }
+    },
+    badge: {
+        marginRight: theme.spacing(1)
+    }
 }));
 
 const Title = styled.h1`
-  font-family: 'Cabin Sketch', cursive;
+    font-family: "Cabin Sketch", cursive;
 `;
 
 function HcsAppBar() {
-  const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+    const history = useHistory();
 
-  const isMenuOpen = Boolean(anchorEl);
-  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+    const isMenuOpen = Boolean(anchorEl);
+    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  // アカウントメニューを表示
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    // アカウントメニューを表示
+    const handleProfileMenuOpen = event => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  // Mobileメニューをクローズ
-  const handleMobileMenuClose = () => {
-    setMobileMoreAnchorEl(null);
-  };
+    // Mobileメニューをクローズ
+    const handleMobileMenuClose = () => {
+        setMobileMoreAnchorEl(null);
+    };
 
-  // アカウントメニューをクローズ
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-    handleMobileMenuClose();
-  };
+    // アカウントメニューをクローズ
+    const handleMenuClose = () => {
+        history.push("/users/1");
+        setAnchorEl(null);
+        handleMobileMenuClose();
+    };
 
-  const handleMessage = () => {
-    () => history.push('/login')
-    handleMobileMenuClose();
-  }
+    const handleMessage = () => {
+        history.push("/login");
+        handleMobileMenuClose();
+    };
 
-  // Logout処理
-  const handleMenuCloseLogout = () => {
-    if(confirm('ログアウトをしますか？')) {
-      setAnchorEl(null);
-      handleMobileMenuClose();
-    }
-  };
+    // Logout処理
+    const handleMenuCloseLogout = () => {
+        if (confirm("ログアウトをしますか？")) {
+            setAnchorEl(null);
+            handleMobileMenuClose();
+        }
+    };
 
-  const handleMobileMenuOpen = (event) => {
-    setMobileMoreAnchorEl(event.currentTarget);
-  };
+    const handleMobileMenuOpen = event => {
+        setMobileMoreAnchorEl(event.currentTarget);
+    };
 
-  // account & profileのメニュー設定
-  const menuId = 'primary-search-account-menu';
-  const renderMenu = (
-    <Menu
-      anchorEl={anchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={menuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
-    >
-      <MenuItem onClick={handleMenuClose}>プロフィール</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My記事</MenuItem>
-      <MenuItem onClick={handleMenuCloseLogout}>ログアウト</MenuItem>
-    </Menu>
-  );
-
-  // Mobileサイズの際のメニュー表示を設定
-  const mobileMenuId = 'primary-search-account-menu-mobile';
-  const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-      <MenuItem>
-        <IconButton aria-label="show 4 new mails" color="inherit" onClick={handleMessage}>
-          <Badge badgeContent={4} color="secondary">
-            <MailIcon />
-          </Badge>
-        </IconButton>
-        <p>メッセージ</p>
-      </MenuItem>
-      <MenuItem>
-        <IconButton aria-label="show 11 new notifications" color="inherit">
-          <Badge badgeContent={11} color="secondary">
-            <NotificationsIcon />
-          </Badge>
-        </IconButton>
-        <p>通知</p>
-      </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
+    // account & profileのメニュー設定
+    const menuId = "primary-search-account-menu";
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
         >
-          <AccountCircle />
-        </IconButton>
-        <p>アカウント</p>
-      </MenuItem>
-    </Menu>
-  );
+            <MenuItem onClick={handleMenuClose}>プロフィール</MenuItem>
+            <MenuItem onClick={handleMenuClose}>My記事</MenuItem>
+            <MenuItem onClick={handleMenuCloseLogout}>ログアウト</MenuItem>
+        </Menu>
+    );
 
-  return (
-    <div className={classes.grow}>
-      <AppBar position="static" className={classes.appBar}>
-        <Toolbar>
-          <MenuDrawer />
-          <div className={classes.title}>
-            <Typography>
-              
-              <Title>HitcHike Community Space</Title>
-    
-            </Typography>
-          </div>
-          <div className={classes.grow} />
-          <div className={classes.sectionDesktop}>
-            <IconButton aria-label="show 4 new mails" color="inherit" className={classes.badge} onClick={handleMessage}>
-              <Badge badgeContent={4} color="secondary">
-                <MailIcon fontSize="large" />
-              </Badge>
-            </IconButton>
-            <IconButton aria-label="show 11 new notifications" color="inherit" className={classes.badge}>
-              <Badge badgeContent={11} color="secondary">
-                <NotificationsIcon fontSize="large" />
-              </Badge>
-            </IconButton>
-            <IconButton
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle fontSize="large" />
-            </IconButton>
-          </div>
-          <div className={classes.sectionMobile}>
-            <IconButton
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreIcon />
-            </IconButton>
-          </div>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </div>
-  );
+    // Mobileサイズの際のメニュー表示を設定
+    const mobileMenuId = "primary-search-account-menu-mobile";
+    const renderMobileMenu = (
+        <Menu
+            anchorEl={mobileMoreAnchorEl}
+            anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            id={mobileMenuId}
+            keepMounted
+            transformOrigin={{ vertical: "top", horizontal: "right" }}
+            open={isMobileMenuOpen}
+            onClose={handleMobileMenuClose}
+        >
+            <MenuItem>
+                <IconButton
+                    aria-label="show 4 new mails"
+                    color="inherit"
+                    onClick={handleMessage}
+                >
+                    <Badge badgeContent={4} color="secondary">
+                        <MailIcon />
+                    </Badge>
+                </IconButton>
+                <p>メッセージ</p>
+            </MenuItem>
+            <MenuItem>
+                <IconButton
+                    aria-label="show 11 new notifications"
+                    color="inherit"
+                >
+                    <Badge badgeContent={11} color="secondary">
+                        <NotificationsIcon />
+                    </Badge>
+                </IconButton>
+                <p>通知</p>
+            </MenuItem>
+            <MenuItem onClick={handleProfileMenuOpen}>
+                <IconButton
+                    aria-label="account of current user"
+                    aria-controls="primary-search-account-menu"
+                    aria-haspopup="true"
+                    color="inherit"
+                >
+                    <AccountCircle />
+                </IconButton>
+                <p>アカウント</p>
+            </MenuItem>
+        </Menu>
+    );
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static" className={classes.appBar}>
+                <Toolbar>
+                    <MenuDrawer />
+                    <div className={classes.title}>
+                        <Typography>
+                            <Title onClick={() => history.push("/")}>
+                                HitcHike Community Space
+                            </Title>
+                        </Typography>
+                    </div>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        <IconButton
+                            aria-label="show 4 new mails"
+                            color="inherit"
+                            className={classes.badge}
+                            onClick={handleMessage}
+                        >
+                            <Badge badgeContent={4} color="secondary">
+                                <MailIcon fontSize="large" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            aria-label="show 11 new notifications"
+                            color="inherit"
+                            className={classes.badge}
+                        >
+                            <Badge badgeContent={11} color="secondary">
+                                <NotificationsIcon fontSize="large" />
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle fontSize="large" />
+                        </IconButton>
+                    </div>
+                    <div className={classes.sectionMobile}>
+                        <IconButton
+                            aria-label="show more"
+                            aria-controls={mobileMenuId}
+                            aria-haspopup="true"
+                            onClick={handleMobileMenuOpen}
+                            color="inherit"
+                        >
+                            <MoreIcon />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            {renderMobileMenu}
+            {renderMenu}
+        </div>
+    );
 }
 
-export default withRouter(HcsAppBar)
+export default withRouter(HcsAppBar);
--- resources/js/components/users/Show.js
+++ resources/js/components/users/Show.js
@@ -1,43 +1,44 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { selectSelectedUser } from './userSlice';
-import _ from 'lodash';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import { Card, CardContent, CardMedia, Typography } from '@material-ui/core';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { selectSelectedUser } from "./userSlice";
+import _ from "lodash";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
+import { withRouter } from "react-router-dom/cjs/react-router-dom.min";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    minWidth: 200,
-    backgroundColor: '#f7fad1',
-    display: 'flex',
-  },
-  details: {
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  content: {
-    flex: '1 0 auto',
-  },
-  cover: {
-    height: 300,
-  },
-  gridContainer: {
-    paddingTop: '10px',
-    paddingBottom: '20px'
-  },
-  userName: {
-      fontSize: '15px',
-  },
+const useStyles = makeStyles(theme => ({
+    root: {
+        minWidth: 200,
+        backgroundColor: "#f7fad1",
+        display: "flex"
+    },
+    details: {
+        display: "flex",
+        flexDirection: "column"
+    },
+    content: {
+        flex: "1 0 auto"
+    },
+    cover: {
+        height: 300
+    },
+    gridContainer: {
+        paddingTop: "10px",
+        paddingBottom: "20px"
+    },
+    userName: {
+        fontSize: "15px"
+    }
 }));
 
-export default function UserShow(props) {
+function UserShow(props) {
     const classes = useStyles();
     const theme = useTheme();
     // stateで管理するユーザ詳細データを使用できるようにローカルのselectedUsers定数に格納
-    const selectedUsers = useSelector(selectSelectedUser)
-    const dispatch = useDispatch()
-    console.log(selectedUsers)
+    const selectedUsers = useSelector(selectSelectedUser);
+    const dispatch = useDispatch();
+    console.log(selectedUsers);
     return (
         <Grid container className={classes.gridContainer} justify="center">
             <Grid item xs={12} sm={6}>
@@ -52,22 +53,28 @@ export default function UserShow(props) {
                     <Grid item xs={6} sm={6}>
                         <div className={classes.details}>
                             <CardContent className={classes.content}>
-                            <Typography component="h5" variant="h5">
-                                Root
-                            </Typography>
-                            <Typography variant="body1" color="textSecondary">
-                                都道府県：大阪府
-                            </Typography>
-                            <Typography variant="body1" color="textSecondary">
-                                生年月日：1991年1月1日
-                            </Typography>
+                                <Typography component="h5" variant="h5">
+                                    Root
+                                </Typography>
+                                <Typography
+                                    variant="body1"
+                                    color="textSecondary"
+                                >
+                                    都道府県：大阪府
+                                </Typography>
+                                <Typography
+                                    variant="body1"
+                                    color="textSecondary"
+                                >
+                                    生年月日：1991年1月1日
+                                </Typography>
                             </CardContent>
                         </div>
                     </Grid>
                 </Card>
-                
             </Grid>
         </Grid>
-    )
+    );
 }
 
+export default withRouter(UserShow);
